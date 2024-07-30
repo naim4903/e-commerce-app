c1791870d6fe9bb16931ee545b8983f2f0bb1a9d
naim4903 <naim4903@gmail.com>
2024-07-30 17:18:45 +0530

    Frontend setup

---
+++ frontend/README.md
@@ -0,0 +1,5 @@
+# To run the app
+1. npm i
+2. npm start
+3. Open your browser and navigate to http://localhost:3000
+
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Welcom to my ecomm web app</h1>
+    </div>
+  );
+};
+
+export default App;
